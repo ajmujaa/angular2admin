6cdcfd24457b059b5aebf0b5193c7971c3250046
Sunil <ssunils2004@gmail.com>
2017-04-17 17:59:38 +0800

    Refactor shared components

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing/dashboard-routing.module';
-import { BarChartComponent } from '../bar-chart/bar-chart.component';
+import { BarChartComponent } from '../shared/bar-chart/bar-chart.component';
 
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 
